767d08e86c605eaf7527a10475f7c6c25b8e3399
Jonathan Birk <jona@jbirk.de>
2021-12-05 12:18:53 +0000

    Fix bugs in build and main

--- build.rs
+++ build.rs
@@ -11,6 +11,7 @@ use std::{
 const FIRST_YEAR: i32 = 2015;
 
 fn main() {
+    println!("cargo:rerun-if-changed=src/years");
     let cookie = include_str!("cookie");
 
     let url = "https://adventofcode.com/".parse::<Url>().unwrap();
@@ -46,12 +47,14 @@ fn main() {
         let year_directory_name = format!("src/years/year{}", year);
         let year_path = Path::new(&year_directory_name);
         fs::create_dir_all(year_path).unwrap();
-        let year_days_mod_path = year_path.join("mod.rs");
-        let year_days_mod_file = if year_days_mod_path.is_file() {
-            None
-        } else {
-            File::create(year_days_mod_path).ok()
-        };
+        let year_mod_path = year_path.join("mod.rs");
+        if !year_mod_path.is_file() && !year_mod_path.exists() {
+            let mut year_mod_file = File::create(year_mod_path).unwrap();
+            let _ = writeln!(year_mod_file, "include!(\"./day_mods.rs\");");
+        }
+        let year_day_mods_path = year_path.join("day_mods.rs");
+        let mut year_day_mods_file = 
+            File::create(year_day_mods_path).unwrap();
 
         if now.year() > year || now.year() == year && now.month() == 12 {
             let max_day = if now.year() > year || now.day() > 24 {
@@ -60,11 +63,6 @@ fn main() {
                 now.day()
             };
 
-            if let Some(mut year_days_mod_file) = year_days_mod_file {
-                (1..=max_day).for_each(|day| {
-                    let _ = writeln!(year_days_mod_file, "pub mod day{:02};", day);
-                });
-            }
             let _ = writeln!(years_mod_file, "        vec![",);
             for day in 1..=max_day {
                 let input_file_name = format!("input{:02}.txt", day);
@@ -73,6 +71,9 @@ fn main() {
                 let sample_path = Path::new(&year_directory_name).join(&sample_file_name);
                 let source_file_name = format!("day{:02}.rs", day);
                 let source_path = Path::new(&year_directory_name).join(&source_file_name);
+                if source_path.is_file() {
+                    let _ = writeln!(year_day_mods_file, "pub mod day{:02};", day);
+                }
 
                 if !input_path.is_file() {
                     let mut input_file = File::create(&input_path).unwrap();
@@ -87,63 +88,28 @@ fn main() {
                     );
                 }
 
-                let _ = writeln!(
-                    years_mod_file,
-                    concat!(
-                        "            (\n",
-                        "                Box::new(year{:04}::day{:02}::DayGen),\n",
-                        "                {},\n",
-                        "                {},\n",
-                        "            ),",
-                    ),
-                    year,
-                    day,
-                    if input_path.is_file() {
-                        format!("include_str!(\"./year{}/{}\")", year, input_file_name)
-                    } else {
-                        format!("\"no input for {day}\"", day = day)
-                    },
-                    if sample_path.is_file() {
-                        format!("include_str!(\"./year{}/{}\")", year, sample_file_name)
-                    } else {
-                        format!("\"no example for {day}\"", day = day)
-                    },
-                );
-                if !source_path.is_file() {
-                    let mut source_file = File::create(source_path).unwrap();
-                    let _ = write!(
-                        source_file,
-                        r#"#![allow(dead_code)]
-pub struct DayGen;
-
-impl crate::DayGen for DayGen {{
-    fn input(&self, input: &str) -> Box<dyn crate::Day> {{
-        let input = input.split('\n').map(|line| line.to_owned()).collect();
-        Box::new(Day::new(input))
-    }}
-}}
-
-type Input = Vec<String>;
-
-struct Day {{
-    input: Input,
-}}
-
-impl Day {{
-    pub fn new(input: Input) -> Self {{
-        Self {{ input }}
-    }}
-}}
-
-impl crate::Day for Day {{
-    fn part1(&self) -> String {{
-        todo!()
-    }}
-
-    fn part2(&self) -> String {{
-        todo!()
-    }}
-}}"#
+                if source_path.is_file() {
+                    let _ = writeln!(
+                        years_mod_file,
+                        concat!(
+                            "            (\n",
+                            "                Box::new(year{:04}::day{:02}::DayGen),\n",
+                            "                {},\n",
+                            "                {},\n",
+                            "            ),",
+                        ),
+                        year,
+                        day,
+                        if input_path.is_file() {
+                            format!("include_str!(\"./year{}/{}\")", year, input_file_name)
+                        } else {
+                            format!("\"no input for {day}\"", day = day)
+                        },
+                        if sample_path.is_file() {
+                            format!("include_str!(\"./year{}/{}\")", year, sample_file_name)
+                        } else {
+                            format!("\"no example for {day}\"", day = day)
+                        },
                     );
                 }
             }
