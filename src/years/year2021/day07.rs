2a502e41847084d73616363e53ec546f8f0f6708
Jonathan Birk <jona@jbirk.de>
2021-12-07 23:06:10 +0000

    2021/07

---
+++ src/years/year2021/day07.rs
@@ -0,0 +1,50 @@
+pub struct DayGen;
+
+impl crate::DayGen for DayGen {
+    fn input(&self, input: &str) -> Box<dyn crate::Day> {
+        let input = input
+            .trim()
+            .split(',')
+            .filter_map(|line| line.parse().ok())
+            .collect();
+        Box::new(Day::new(input))
+    }
+}
+
+type Input = Vec<i64>;
+
+struct Day {
+    input: Input,
+}
+
+impl Day {
+    pub fn new(input: Input) -> Self {
+        Self { input }
+    }
+}
+
+impl crate::Day for Day {
+    fn part1(&self) -> String {
+        let mut sorted = self.input.clone();
+        sorted.sort_unstable();
+        let median = dbg!(sorted[sorted.len() / 2]);
+        let fuel: i64 = self.input.iter().map(|p| dbg!(p - median).abs()).sum();
+        fuel.to_string()
+    }
+
+    fn part2(&self) -> String {
+        let min = *self.input.iter().min().unwrap();
+        let max = *self.input.iter().max().unwrap();
+        let fuel: i64 = (min..max)
+            .map(|i| {
+                self.input
+                    .iter()
+                    .map(|p| (p - i).abs())
+                    .map(|d| d * (d + 1) / 2)
+                    .sum()
+            })
+            .min()
+            .unwrap();
+        fuel.to_string()
+    }
+}
