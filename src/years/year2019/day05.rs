c05583ec18cd5d9615c6532c2222026ee792c3eb
Jonathan Birk <jona@jbirk.de>
2021-12-07 23:06:33 +0000

    2019/05/1

--- src/years/year2019/day05.rs
+++ src/years/year2019/day05.rs
@@ -5,7 +5,7 @@ impl crate::DayGen for DayGen {
     fn input(&self, input: &str) -> Box<dyn crate::Day> {
         let input = input
             .split(',')
-            .filter_map(|line| line.trim().parse().ok())
+            .filter_map(|line| line.parse().ok())
             .collect();
         Box::new(Day::new(input))
     }
@@ -26,24 +26,12 @@ impl Day {
 impl crate::Day for Day {
     fn part1(&self) -> String {
         let mut machine = Machine::from(&self.input);
-        machine.memory[1] = 12;
-        machine.memory[2] = 2;
+        machine.input(vec![1i64]);
         machine.run();
-        format!("{}", machine.memory[0])
+        format!("{}", machine.output().iter().last().unwrap())
     }
 
     fn part2(&self) -> String {
-        for noun in 0..=99 {
-            for verb in 0..=99 {
-                let mut machine = Machine::from(&self.input);
-                machine.memory[1] = noun;
-                machine.memory[2] = verb;
-                machine.run();
-                if machine.memory[0] == 19690720 {
-                    return format!("{}", 100 * noun + verb);
-                }
-            }
-        }
-        unreachable!();
+        todo!()
     }
 }
--- src/years/year2019/intcode.rs
+++ src/years/year2019/intcode.rs
@@ -1,53 +1,169 @@
+use std::{
+    collections::VecDeque,
+    fmt::{Debug, Formatter, Result},
+    ops::{Index, IndexMut},
+};
+
+type Atom = i64;
 pub struct Machine {
-    pub memory: Vec<i64>,
+    pub memory: Memory,
     ip: usize,
     pub halt: bool,
+    input: VecDeque<Atom>,
+    output: Vec<Atom>,
+}
+impl Debug for Machine {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.debug_struct("Machine")
+            .field("memory", &self.memory)
+            .field("ip", &self.ip)
+            .field("halt", &self.halt)
+            .field("output", &self.output)
+            .finish_non_exhaustive()
+    }
 }
 
-impl Machine {
-    pub fn step(&mut self) {
-        let opcode = self.memory[self.ip];
-        let (in1, in2, out) = (
-            self.memory[self.ip + 1] as usize,
-            self.memory[self.ip + 2] as usize,
-            self.memory[self.ip + 3] as usize,
-        );
-        self.ip += 4;
-        match opcode {
-            1 => self.memory[out] = self.memory[in1] + self.memory[in2],
-            2 => self.memory[out] = self.memory[in1] * self.memory[in2],
-            99 => self.halt = true,
+#[derive(Clone, Debug)]
+pub struct Memory(Vec<Atom>);
+
+impl Memory {
+    fn get(&self, param: Parameter) -> i64 {
+        match param {
+            Parameter::Immediate(i) => i,
+            Parameter::Position(p) => self.0[p],
+        }
+    }
+
+    fn get_mut(&mut self, param: Parameter) -> &mut i64 {
+        match param {
+            Parameter::Position(p) => &mut self.0[p],
             _ => unimplemented!(),
         }
+    }
+}
+
+impl Index<usize> for Memory {
+    type Output = Atom;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl IndexMut<usize> for Memory {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Parameter {
+    Immediate(Atom),
+    Position(usize),
+}
+
+#[derive(Copy, Clone, Debug)]
+enum OpCode {
+    Add(Parameter, Parameter, Parameter),
+    Mul(Parameter, Parameter, Parameter),
+    Input(Parameter),
+    Output(Parameter),
+    Halt,
+}
+
+impl OpCode {
+    fn len(&self) -> usize {
+        match self {
+            Self::Add(..) | Self::Mul(..) => 4,
+            Self::Input(..) | Self::Output(..) => 2,
+            Self::Halt => 0,
+        }
+    }
+}
+impl Machine {
+    pub fn step(&mut self) {
+        let op_code = self.op_code(self.ip);
+        self.ip += op_code.len();
+        use OpCode::*;
+        match op_code {
+            Add(in1, in2, out) => {
+                *self.memory.get_mut(out) = self.memory.get(in1) + self.memory.get(in2);
+            }
+            Mul(in1, in2, out) => {
+                *self.memory.get_mut(out) = self.memory.get(in1) * self.memory.get(in2);
+            }
+            Halt => {
+                self.halt = true;
+            }
+            Input(out) => {
+                if let Some(val) = self.input.pop_front() {
+                    *self.memory.get_mut(out) = val
+                }
+            }
+            Output(input) => self.output.push(self.memory.get(input)),
+        };
     }
     pub fn run(&mut self) {
         while !self.halt {
             self.step();
         }
     }
-    pub fn input(&mut self, input1: i64, input2: i64) {
-        self.memory[1] = input1;
-        self.memory[2] = input2;
+    pub fn input(&mut self, input: Vec<Atom>) {
+        self.input.reserve(input.len());
+        input.into_iter().for_each(|v| self.input.push_back(v));
     }
-    pub fn output(&self) -> i64 {
-        self.memory[0]
+    pub fn output(&self) -> &[Atom] {
+        &self.output[..]
     }
-}
-impl From<Vec<i64>> for Machine {
-    fn from(memory: Vec<i64>) -> Self {
-        Self {
-            memory,
-            ip: 0,
-            halt: false,
+
+    fn op_code(&self, ip: usize) -> OpCode {
+        let op_code = self.memory[ip] % 100;
+        let param_mode1 = (self.memory[ip] / 100 % 10) as u8;
+        let param_mode2 = (self.memory[ip] / 1000 % 10) as u8;
+        let param_mode3 = (self.memory[ip] / 10000 % 10) as u8;
+        match op_code {
+            1 => OpCode::Add(
+                Parameter::new(param_mode1, self.memory[ip + 1]),
+                Parameter::new(param_mode2, self.memory[ip + 2]),
+                Parameter::new(param_mode3, self.memory[ip + 3]),
+            ),
+            2 => OpCode::Mul(
+                Parameter::new(param_mode1, self.memory[ip + 1]),
+                Parameter::new(param_mode2, self.memory[ip + 2]),
+                Parameter::new(param_mode3, self.memory[ip + 3]),
+            ),
+            3 => OpCode::Input(Parameter::new(0, self.memory[ip + 1])),
+            4 => OpCode::Output(Parameter::new(param_mode1, self.memory[ip + 1])),
+            99 => OpCode::Halt,
+            _ => unimplemented!(),
         }
     }
 }
-impl From<&Vec<i64>> for Machine {
-    fn from(memory: &Vec<i64>) -> Self {
+
+impl From<Vec<Atom>> for Machine {
+    fn from(memory: Vec<Atom>) -> Self {
         Self {
-            memory: memory.to_owned(),
+            memory: Memory(memory),
             ip: 0,
             halt: false,
+            input: VecDeque::new(),
+            output: Vec::new(),
+        }
+    }
+}
+
+impl From<&Vec<Atom>> for Machine {
+    fn from(memory: &Vec<Atom>) -> Self {
+        Self::from(memory.to_owned())
+    }
+}
+
+impl Parameter {
+    fn new(param_mode: u8, ip: Atom) -> Parameter {
+        match param_mode {
+            0 if ip > 0 => Self::Position(ip as usize),
+            1 => Self::Immediate(ip),
+            _ => unimplemented!(),
         }
     }
 }
