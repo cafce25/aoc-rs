e30de1f62b8ddc02eb7869e0d2eb3c77ede93829
Jonathan Birk <jona@jbirk.de>
2021-12-08 14:50:27 +0000

    2021/08/2

--- src/years/year2021/day08.rs
+++ src/years/year2021/day08.rs
@@ -1,5 +1,9 @@
 #![allow(dead_code)]
-use std::str::FromStr;
+use std::{
+    collections::{HashMap, HashSet},
+    hash::{Hash, Hasher},
+    str::FromStr,
+};
 
 pub struct DayGen;
 
@@ -26,8 +30,87 @@ impl Digits {
     fn outputs<'a>(&'a self) -> impl Iterator<Item = &Digit> + 'a {
         self.output.iter()
     }
+
     fn inputs<'a>(&'a self) -> impl Iterator<Item = &Digit> + 'a {
         self.input.iter()
+    }
+
+    fn value(&self) -> u64 {
+        let mut input: HashSet<_> = self.input.iter().collect();
+        let mut map = HashMap::new();
+
+        if let Some(digit) = input.iter().find(|e| e.len() == 2) {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(1, digit);
+        };
+
+        if let Some(digit) = input.iter().find(|e| e.len() == 4) {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(4, digit);
+        };
+
+        if let Some(digit) = input.iter().find(|e| e.len() == 3) {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(7, digit);
+        };
+
+        if let Some(digit) = input.iter().find(|e| e.len() == 7) {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(8, digit);
+        };
+
+        if let Some(digit) = input
+            .iter()
+            .find(|e| e.len() == 5 && map[&1].wires.iter().all(|w| e.wires.contains(w)))
+        {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(3, digit);
+        }
+
+        if let Some(digit) = input
+            .iter()
+            .find(|e| e.len() == 6 && map[&4].wires.iter().all(|w| e.wires.contains(w)))
+        {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(9, digit);
+        }
+
+        if let Some(digit) = input
+            .iter()
+            .find(|e| e.len() == 5 && e.wires.iter().all(|w| map[&9].wires.contains(w)))
+        {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(5, digit);
+        }
+
+        if let Some(digit) = input.iter().find(|e| e.len() == 5) {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(2, digit);
+        }
+
+        if let Some(digit) = input
+            .iter()
+            .find(|e| map[&5].wires.iter().all(|w| e.wires.contains(w)))
+        {
+            let digit = digit.clone();
+            input.remove(digit);
+            map.insert(6, digit);
+        }
+
+        input.drain().for_each(|e| {
+            map.insert(0, e);
+        });
+
+        let map: HashMap<_, _> = map.into_iter().map(|(k, v)| (v, k)).collect();
+        self.output.iter().fold(0, |n, d| map[d] + 10*n)
     }
 }
 
@@ -47,10 +130,11 @@ impl FromStr for Digits {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct Digit {
-    wires: Vec<Wire>,
+    wires: HashSet<Wire>,
 }
+
 impl Digit {
     fn len(&self) -> usize {
         self.wires.len()
@@ -61,7 +145,7 @@ impl FromStr for Digit {
     type Err = anyhow::Error;
     fn from_str(s: &str) -> anyhow::Result<Self> {
         use Wire::*;
-        let wires: Result<Vec<_>, _> = s
+        let wires: Result<HashSet<_>, _> = s
             .chars()
             .map(|c| {
                 Ok(match c {
@@ -79,9 +163,14 @@ impl FromStr for Digit {
         Ok(Self { wires: wires? })
     }
 }
-impl Digit {}
 
-#[derive(Copy, Clone, Debug)]
+impl Hash for Digit {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_u8(self.wires.iter().map(|a| 1 << (*a as u8)).sum::<u8>())
+    }
+}
+
+#[derive(Copy, Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Wire {
     A,
     B,
@@ -113,6 +202,10 @@ impl crate::Day for Day {
     }
 
     fn part2(&self) -> String {
-        todo!()
+        self.input
+            .iter()
+            .map(Digits::value)
+            .sum::<u64>()
+            .to_string()
     }
 }
