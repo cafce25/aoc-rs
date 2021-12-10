135851cc9bb6735378a7e00176eee212fa36f04d
Jonathan Birk <jona@jbirk.de>
2021-12-10 12:49:43 +0000

    Enable Day to hold on to references of the input

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub const MIN_YEAR: i32 = 2015;
 pub use years::YEARS;
 
 pub trait DayGen {
-    fn input(&self, input: &str) -> Box<dyn Day>;
+    fn input<'a>(&'a self, input: &'a str) -> Box<dyn Day + 'a>;
 }
 pub trait Day {
     fn part1(&self) -> String;
