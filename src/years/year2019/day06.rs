fe76d59b9fde9f1d7f30062b2599adcc0f2ba36b
Jonathan Birk <jona@jbirk.de>
2021-12-10 13:35:01 +0000

    2019/06/2

--- src/years/year2019/day06.rs
+++ src/years/year2019/day06.rs
@@ -26,21 +26,36 @@ impl<'a> Day<'a> {
             .collect();
         Self { input }
     }
+
+    pub fn suborbits(&self) -> HashMap<&str, Vec<&str>> {
+        let mut sorbits = HashMap::new();
+        self.input.keys().for_each(|outmost| {
+            if !sorbits.contains_key(outmost) {
+                let mut rev_orbit = Vec::new();
+                let mut current = outmost;
+                rev_orbit.push(*current);
+                while let Some(c) = self.input.get(current) {
+                    current = c;
+                    rev_orbit.push(current);
+                }
+                sorbits.insert(*outmost, rev_orbit.into_iter().skip(1).rev().collect());
+            }
+        });
+        sorbits
+    }
 }
 
 impl<'a> crate::Day for Day<'a> {
     fn part1(&self) -> String {
-        self.input.keys().map(|mut satelite| {
-            let mut orbits = 0;
-            while let Some(sat) = self.input.get(satelite) {
-                satelite = sat;
-                orbits += 1;
-            }
-            orbits
-        }).sum::<usize>().to_string()
+        self.suborbits().values().map(Vec::len).sum::<usize>().to_string()
     }
 
     fn part2(&self) -> String {
-        todo!()
+        let orbits = self.suborbits();
+        let san = &orbits["SAN"];
+        let you = &orbits["YOU"];
+        let mut i = 0;
+        while san[0..=i] == you[0..=i] { i += 1 }
+        (san.len() + you.len() - 2 * i).to_string()
     }
 }
